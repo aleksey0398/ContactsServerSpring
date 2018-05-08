42995b85b6d71890d5291bc7e17655afebb78e1e
Aleksey Mitkin <aleksey0398@icloud.com>
2018-05-08 20:25:00 +0500

    Initial commit

---
+++ src/main/java/com/samsung/contacts_server_spring/ContactsServerSpringApplication.java
@@ -0,0 +1,12 @@
+package com.samsung.contacts_server_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsServerSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/samsung/contacts_server_spring/ContactsServerSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samsung.contacts_server_spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactsServerSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
